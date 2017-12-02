a07cda7640d2b21633eb72f8db4faa0d08c04f8f
Sparsha Saha <saha.sparsha@gmail.com>
2017-12-03 02:42:16 +0530

    Server fixed

--- Routes.js
+++ Routes.js
@@ -241,7 +241,7 @@ app.get("/getmail",(req,res)=>{
 
                 }
                 else {
-                  Log.d(resp[0].email);
+                  Log(resp[0].email);
                   res.json(resp[0].email+"");
                 }
 
