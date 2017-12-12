d9bdde4af3a11d65e2eadd1305b506e5c6e67009
sridharswain <sridharswain25@hotmail.com>
2017-12-12 18:47:27 +0530

    Client Setup and SplashScreen Complete

---
+++ Client/Confez/index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import SplashScreen from './src/screens/SplashScreen'
+import Login from './src/screens/Login'
+
+AppRegistry.registerComponent('Confez', () => Login);
