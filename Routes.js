8cdb2241eb0d831ff90ca7658202d6adad792e1b
SparshaSaha <saha.sparsha@gmail.com>
2017-12-13 02:22:10 +0530

    Server fixed

--- Routes.js
+++ Routes.js
@@ -142,7 +142,7 @@ module.exports= function(mongo){
                   photo_link:resp[0].photo_link
                 };
 
-                socket.emit('signin_reply',JSON.stringify(temp));
+                socket.emit('signin_reply',(temp));
               }
           });
         });
